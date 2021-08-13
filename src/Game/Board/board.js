2ba5206ed78e274499fdf6af3174a78f267937eb
masonlai <123hosumlai@gmail.com>
2021-08-13 16:04:20 +0800

    dynamic winner calculator

--- src/Game/Board/board.js
+++ src/Game/Board/board.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Square from './BoardComponent/Square';
-import calculateWinner from './calculateWinner';
+import winnerCalculator from './calculateWinner';
+
+
 
 class Board extends React.Component {
     constructor(props) {
@@ -9,11 +11,15 @@ class Board extends React.Component {
             squares: Array(9).fill(null),
             Xturn: true,
             turnPlayer: 'X',
+            winCondition: 3,
+            numOfBoardRows: 3,
+            winner: null
+
         }
     }
 
     handleClick(i) {
-        if (this.isOverwiriting(i) || calculateWinner(this.state.squares)) return;
+        if (this.isOverwiriting(i) || this.state.winner) return;
 
         let squaresCopy = this.state.squares.slice();
         if (this.state.Xturn) {
@@ -24,46 +30,53 @@ class Board extends React.Component {
             this.setState({ Xturn: true, turnPlayer: 'X' })
         }
         this.setState({ squares: squaresCopy })
+
+        const WinnerCalculator = new winnerCalculator(squaresCopy, this.state.winCondition, this.state.numOfBoardRows);
+        const winner = WinnerCalculator.getWinner();
+        this.setState({
+            winner: winner
+        })
     }
 
-    returnCurrPlayer(){
-        return this.state.Xturn ? 'X':'O';
+    returnCurrPlayer() {
+        return this.state.Xturn ? 'X' : 'O';
     }
 
     isOverwiriting(i) {
         return this.state.squares[i] != null
     }
 
-
     renderSquare(i) {
         return <Square selectedByUser={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
+        const winner = this.state.winner;
         let status;
         if (winner) {
-          status = 'Winner: ' + winner;
+            status = 'Winner: ' + winner;
         } else {
-          status = 'Next player: ' + this.state.turnPlayer;
+            status = 'Next player: ' + this.state.turnPlayer;
         }
         return (
             <div>
                 <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
+                <div>
+                    <div className="board-row">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                    </div>
                 </div>
             </div>
         );
